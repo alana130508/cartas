ec84823a0fe51ece0654c83571d4137f1ac5c28f
alana130508 <00001105683175sp@al.educacao.sp.gov.br>
2024-11-22 16:16:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Historia', 
+    'O que foi iluminismo?',
+    'Brasília'
+)
+
+criaCartao(
+    'Geografia', 
+    'O que sâo placas tectônicas?',
+    'Estruturas da crosta terrestre'
+)
+
+criaCartao(
+    'Biologia', 
+    'O que è gene?',
+    'Unidade de hereditariedade'
+)
+
+criaCartao(
+    'Matemática',
+    'O que è logaritmo?',
+    'Inverso da exponenciaçâo'
+)
+
+criaCartao(
+    'història',
+    'Quem foi Karl Marx?',
+    'Filòsofo socialista'
+)
+
+criaCartao(
+    'Matemáticas',
+    'Quantos nùmeros primos existem entre 0 e 100?',
+    '25'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual gás é liberado no processo da respirção celular?',
+    'Gás carbonico'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são biomas?',
+    'Conjuntos de ecossistemas'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é ecologia?',
+    'Estudos dos seres e o ambiente'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico com simbolo Na?',
+    'Sódio'
+)
+
+criaCartao(
+    'Potuguễs',
+    'O que é artigo?',
+    'Define ou indertimina o substantivos'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é núḿero primo?',
+    'Divisivel po 1 e ele mesmos'
+)
